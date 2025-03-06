fdd6305d83b42454d3f5448639fa912754507797
esmagulbas01 <esmagulbas@posta.mu.edu.tr>
2025-03-06 13:16:32 +0300

    week3

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,8 +1,34 @@
+import java.sql.SQLOutput;
+
 public class FindGrade{
 
     public static void main(String[] args){
+        int length_of_args = args.length;
+        if (length_of_args == 0) {
+            System.out.println("you should give a score");
+        }
+        else{
+            int grade = Integer.parseInt(args[0]);
 
-        //Get the score from the user as a file parameter
+
+
+            if (grade >= 90 && grade<100){
+                System.out.println("Grade:A");
+            } else if (grade>=80 && grade<89) {
+                System.out.println("Grade :B");
+            } else if (grade >= 70 && grade <79) {
+                System.out.println("Grade : C");
+            } else if (grade >= 60 && grade <69) {
+                System.out.println("Grade :D");
+            } else if (grade >= 0 && grade <59){
+                System.out.println("Grade :F");
+            } else {
+                System.out.println("invalid Score ");
+            }
+
+
+
+    }
         //Calculate the grade based on the score
         //Print the grade to the console
         // 90 - 100 -> A
